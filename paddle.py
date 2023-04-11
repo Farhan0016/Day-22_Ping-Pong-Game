4e6d5f926fb6d64552de8819b5c46cd112012374
Muhammad Farhan <103308228+Farhan0016@users.noreply.github.com>
2023-04-11 07:42:12 +0500

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,21 +1,5 @@
 from turtle import Turtle
 
-##class Paddle:
-##    def __init__(self, position):
-##        self.paddle = Turtle(shape='square')
-##        self.paddle.penup()
-##        self.paddle.color('white')
-##        self.paddle.setheading(90)
-##        self.paddle.shapesize(stretch_wid=1, stretch_len=5)
-##        self.paddle.goto(position)
-##
-##    def move_upward(self):
-##        #self.paddle.forward(20)
-##        self.paddle.goto(self.paddle.xcor(), self.paddle.ycor()+20)
-##
-##    def move_downward(self):
-##        #self.paddle.forward(-20)
-##        self.paddle.goto(self.paddle.xcor(), self.paddle.ycor()-20)
 
 class Paddle(Turtle):
     def __init__(self, position):
@@ -29,8 +13,6 @@ class Paddle(Turtle):
 
     def move_upward(self):
         self.forward(20)
-##        self.paddle.goto(self.paddle.xcor(), self.paddle.ycor()+20)
 
     def move_downward(self):
         self.forward(-20)
-##        self.paddle.goto(self.paddle.xcor(), self.paddle.ycor()-20)
