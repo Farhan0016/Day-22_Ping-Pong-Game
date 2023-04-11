dca3f3a9aa5057dd8f6f7c1752e00474b396225c
Muhammad Farhan <103308228+Farhan0016@users.noreply.github.com>
2023-04-11 07:38:38 +0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -6,21 +6,17 @@ class Ball(Turtle):
         self.shape('circle')
         self.color('white')
         self.penup()
-        self.setheading(45)
         self.x_move = 10
         self.y_move = 10
         self.move_speed = 0.1
-##        self.movement = 10
 
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
-##        self.forward(self.movement)
 
     def wall_bounce(self):
         self.y_move *= -1
-##        self.movement *= -1
 
     def paddle_bounce(self):
         self.x_move *= -1
